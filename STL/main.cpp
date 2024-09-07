a2822e5f643c0540e13c601248a09d9cc8634788
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-09-07 23:56:29 +0500

    std::vector in progress

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,8 +1,68 @@
 #include<iostream>
-using namespace std;
+#include<array>
+#include<vector>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+#define delimiter "\n------------------------------------------\n"
+
+//#define STL_ARRAY
+#define STL_VECTOR
+
+template<typename T> void vector_info(const std::vector<T>& vec)
+{
+	cout << endl;
+	cout << "Size:" << vec.size() << endl;
+	cout << "Capacity:" << vec.capacity() << endl;
+	cout << "MaxSize:" << vec.max_size() << endl;
+}
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "Hello STL" << endl;
+#ifdef STL_ARRAY
+	//array-это хранит данные ввиде статического массива.
+	//При создании 'array', кроме типа хранимых значений, так же оюязательно нужно указать 
+	//количество хранимых значений
+	const int N = 5;
+	std::array<int, N> arr = { 3,5,8,13,21};
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+#endif // STL_ARRAY
+
+#ifdef STL_VECTOR
+	//vector-это контейнер, который хранит данные ввиде динамического массива.
+	//При создании вектора обязательно задают только тип хранимых элементов 
+	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << tab;
+	}
+	vector_info(vec);
+	vec.push_back(55);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << tab;
+	}
+	cout << endl;
+	vector_info(vec);
+	vec.reserve(24);
+	vector_info(vec);
+	vec.shrink_to_fit();
+	vector_info(vec);
+	vec.resize(8);
+	for (std::vector<int> ::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	vector_info(vec);
+#endif // STL_VECTOR
+
 }
