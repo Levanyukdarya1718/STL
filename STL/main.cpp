4fff8c04145b58f002f40fd12e17f752af22e114
Дашка Леванюк <levanyuk.darja@yandex.ru>
2024-09-12 20:36:45 +0500

    HomeWorkVectorList

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<array>
 #include<vector>
+#include<list>
+#include<iterator>
 
 using std::cin;
 using std::cout;
@@ -11,6 +13,7 @@ using std::endl;
 
 //#define STL_ARRAY
 #define STL_VECTOR
+//#define STL_LIST
 
 template<typename T> void vector_info(const std::vector<T>& vec)
 {
@@ -63,6 +66,51 @@ void main()
 	}
 	cout << endl;
 	vector_info(vec);
+	int index;
+	int value;
+	cout << "введите позицию добавляемого элемента: "; cin >> index;
+	cout << "введите значение добавляемого элемента: "; cin >> value;
+	std::vector<int>::iterator position_insert = vec.begin();
+	std::advance(position_insert, index);
+	vec.insert(position_insert, value);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << tab;
+	}
+	cout << endl;
+	cout << "введите позицию удаляемого элемента: "; cin >> index;
+	std::vector<int>::iterator position_erase = vec.begin() ;
+	advance(position_erase, index);
+	vec.erase(position_erase);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i) << tab;
+	}
 #endif // STL_VECTOR
+
+#ifdef STL_LIST
+	std::list<int> list = { 3,5,8,13,21,34,55 };
+	for (std::list<int>::iterator i = list.begin();
+		i != list.end();
+		i++)cout << *i << tab;cout << endl;
+	int index;
+	int value;
+	cout << "введите позицию добавляемого элемента: "; cin >> index;
+	cout << "введите значение добавляемого элемента: "; cin >> value;
+	std::list<int>::iterator position_insert = list.begin();
+	std::advance(position_insert, index);
+	list.insert(position_insert, value);
+	for (std::list<int>::iterator i = list.begin();
+		i != list.end();
+		i++)cout << *i << tab; cout << endl;
+	cout << "введите позицию удаляемого элемента: "; cin >> index;
+	std::list<int>:: iterator position_erase = list.begin();
+	advance(position_erase, index);
+	list.erase(position_erase);
+	for (std::list<int>::iterator i = list.begin();
+		i != list.end();
+		i++)cout << *i << tab;cout << endl;
+#endif // STL_LIST
+
 
 }
